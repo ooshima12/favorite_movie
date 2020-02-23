88ee8dd8eddb90299b9b7ad85c5646299c990720
ooshima12 <maachan.s2.0622@gmail.com>
2020-02-23 19:10:23 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root  'movies#index'
+  resources :users, only: [:index, :edit, :update]
+  resources :movies, only: [:index, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
